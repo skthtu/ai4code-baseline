105403546e86461887957dc7009efcb42604f1e5
skthtu <63440640+skthtu@users.noreply.github.com>
2022-07-12 22:21:07 +0900

    Update model.py

--- code/model.py
+++ code/model.py
@@ -8,10 +8,12 @@ class MarkdownModel(nn.Module):
     def __init__(self, model_path):
         super(MarkdownModel, self).__init__()
         self.model = AutoModel.from_pretrained(model_path)
-        self.top = nn.Linear(769, 1)
+        self.top = nn.LSTM(input_size = 769, hidden_size = 100, num_layers = 1)
 
     def forward(self, ids, mask, fts):
         x = self.model(ids, mask)[0]
         x = torch.cat((x[:, 0, :], fts), 1)
-        x = self.top(x)
+        h0 = torch.randn(1, 100)
+        c0 = torch.randn(1, 100)
+        x = self.top(x, (h0, c0))
         return x
