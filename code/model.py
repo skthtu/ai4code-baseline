7d10f8fdfc4664787dec1f0efd67e51d85aa7ac8
skthtu <63440640+skthtu@users.noreply.github.com>
2022-07-17 14:30:42 +0900

    Update model.py

--- code/model.py
+++ code/model.py
@@ -4,14 +4,21 @@ class MarkdownModel(nn.Module):
         super(MarkdownModel, self).__init__()
         self.model = AutoModel.from_pretrained(model_path)
         self.top = nn.Linear(769, 1)
-        self.cnn1 = nn.Conv1d(768, 256, kernel_size=2, padding=1)
+        self.cnn1 = nn.Conv1d(769, 256, kernel_size=2, padding=1)
         self.cnn2 = nn.Conv1d(256, 1, kernel_size=2, padding=1)
 
     def forward(self,ids, mask, fts):
-        outputs = self.model(ids, mask)
-        last_hidden_state = outputs['last_hidden_state'].permute(0, 2, 1)
-        cnn_embeddings = F.relu(self.cnn1(last_hidden_state))
-        cnn_embeddings = self.cnn2(cnn_embeddings)
-        logits, _ = torch.max(cnn_embeddings, 2)
-
+        #outputs = self.model(ids, mask)
+        #last_hidden_state = outputs['last_hidden_state'].permute(0, 2, 1)
+        #cnn_embeddings = F.relu(self.cnn1(last_hidden_state))
+        #cnn_embeddings = self.cnn2(cnn_embeddings)
+        #logits, _ = torch.max(cnn_embeddings, 2)
+        
+        x = self.model(ids, mask)[0] #batchsize*文章の長さ*特徴次元
+        x = x.permute(0, 2, 1) #batchsize* 特徴次元 * 文章の長さ
+        x = torch.cat((x, fts), 1) 
+        x = F.relu(self.cnn1(x))
+        x = self.cnn2(x)
+        logits, _ = torch.max(x, 2)
+        
         return logits
