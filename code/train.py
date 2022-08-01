ac2d1e11b7c4da0cf054f785166db3fb63bcd946
skthtu <63440640+skthtu@users.noreply.github.com>
2022-08-01 18:01:09 +0900

    Update train.py

--- code/train.py
+++ code/train.py
@@ -77,7 +77,6 @@ def validate(model, val_loader):
 
     return np.concatenate(labels), np.concatenate(preds)
 
-
 def train(model, train_loader, val_loader, epochs):
     np.random.seed(0)
     # Creating optimizer and lr schedulers
@@ -100,7 +99,12 @@ def train(model, train_loader, val_loader, epochs):
 
     criterion = torch.nn.L1Loss() #Loss関数
     scaler = torch.cuda.amp.GradScaler()
-
+    
+    awp = AWP(model,
+          optimizer,
+          scaler=scaler
+             )
+    kenndall_score = 0
     for e in range(epochs):
         model.train()
         tbar = tqdm(train_loader, file=sys.stdout)
@@ -115,12 +119,17 @@ def train(model, train_loader, val_loader, epochs):
                 pred = model(*inputs)
                 loss = criterion(pred, target)
             scaler.scale(loss).backward() #逆伝播
+            
+            if  kenndall_score > 0.78:
+                awp.attack_backward(*inputs,target, e) 
+                
             if idx % args.accumulation_steps == 0 or idx == len(tbar) - 1: #accumulation_stepsごとに予測を抜ける and 最後に抜ける。
                 scaler.step(optimizer)
                 scaler.update()
                 optimizer.zero_grad()
                 scheduler.step()
-
+  
+                
             loss_list.append(loss.detach().cpu().item()) #ロスを取り出す。
             preds.append(pred.detach().cpu().numpy().ravel()) #予測値を取り出す
             labels.append(target.detach().cpu().numpy().ravel()) #ラベルを取り出す
@@ -134,10 +143,85 @@ def train(model, train_loader, val_loader, epochs):
         val_df["pred"] = val_df.groupby(["id", "cell_type"])["rank"].rank(pct=True)
         val_df.loc[val_df["cell_type"] == "markdown", "pred"] = y_pred
         y_dummy = val_df.sort_values("pred").groupby('id')['cell_id'].apply(list)
+        kenndall_score = kendall_tau(df_orders.loc[y_dummy.index], y_dummy)
         print("Preds score", kendall_tau(df_orders.loc[y_dummy.index], y_dummy))
         torch.save(model.state_dict(), "./outputs/model.bin")
 
     return model, y_pred
+
+
+class AWP:
+    def __init__(
+        self,
+        model,
+        optimizer,
+        adv_param="weight",
+        adv_lr=1,
+        adv_eps=0.2,
+        start_epoch=0,
+        adv_step=1,
+        scaler=None
+    ):
+        self.model = model
+        self.optimizer = optimizer
+        self.adv_param = adv_param
+        self.adv_lr = adv_lr
+        self.adv_eps = adv_eps
+        self.start_epoch = start_epoch
+        self.adv_step = adv_step
+        self.backup = {}
+        self.backup_eps = {}
+        self.scaler = scaler
+
+    def attack_backward(self, x, y, epoch):
+        if (self.adv_lr == 0) or (epoch < self.start_epoch):
+            return None
+
+        self._save() 
+        for i in range(self.adv_step):
+            self._attack_step() 
+            with torch.cuda.amp.autocast():
+                tr_logits = self.model(x)
+                adv_loss =  criterion(tr_logits, y)
+                adv_loss = adv_loss.mean()
+            self.optimizer.zero_grad()
+            self.scaler.scale(adv_loss).backward()
+            
+        self._restore()
+
+    def _attack_step(self):
+        e = 1e-6
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and param.grad is not None and self.adv_param in name:
+                norm1 = torch.norm(param.grad)
+                norm2 = torch.norm(param.data.detach())
+                if norm1 != 0 and not torch.isnan(norm1):
+                    r_at = self.adv_lr * param.grad / (norm1 + e) * (norm2 + e)
+                    param.data.add_(r_at)
+                    param.data = torch.min(
+                        torch.max(param.data, self.backup_eps[name][0]), self.backup_eps[name][1]
+                    )
+                # param.data.clamp_(*self.backup_eps[name])
+
+    def _save(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and param.grad is not None and self.adv_param in name:
+                if name not in self.backup:
+                    self.backup[name] = param.data.clone()
+                    grad_eps = self.adv_eps * param.abs().detach()
+                    self.backup_eps[name] = (
+                        self.backup[name] - grad_eps,
+                        self.backup[name] + grad_eps,
+                    )
+
+    def _restore(self,):
+        for name, param in self.model.named_parameters():
+            if name in self.backup:
+                param.data = self.backup[name]
+        self.backup = {}
+        self.backup_eps = {}
+
+
 
 
 model = MarkdownModel(args.model_name_or_path)
